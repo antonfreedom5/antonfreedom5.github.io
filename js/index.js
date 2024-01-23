ce990c5770f8832d56f7dd2a0fefb0beffd274d8
Anton Yadchenko <antonfreedom5@gmail.com>
2024-01-23 22:21:41 +0300

    add tg script

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const tg = window.Telegram.WebApp;
+
+const machines = {
+    "a": ["a", "b", "c"],
+    "b": ["a", "b", "c"],
+    "c": ["a", "b", "c"],
+};
+
+tg.expand(); //расширяем на все окно
+
+tg.MainButton.text = "Changed Text"; //изменяем текст кнопки
+tg.MainButton.setText("Changed Text1"); //изменяем текст кнопки иначе
+tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
+tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
+tg.MainButton.setParams({"color": "#143F6B"}); //так изменяются все параметры
+tg.MainButton.show();
+
+console.log(tg.initData);
