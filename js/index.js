5b696fd22469575489de9dd304fe8e95ce6fd805
Anton Yadchenko <antonfreedom5@gmail.com>
2024-01-24 09:20:01 +0300

    add images

--- js/index.js
+++ js/index.js
@@ -9,10 +9,8 @@ const machines = {
 tg.expand(); //расширяем на все окно
 
 tg.MainButton.text = "Сделать заказ"; //изменяем текст кнопки
-tg.MainButton.setText("Changed Text1"); //изменяем текст кнопки иначе
 tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
 tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
-tg.MainButton.setParams({"color": "#143F6B"}); //так изменяются все параметры
 tg.MainButton.show();
 
 console.log(tg.initData);
