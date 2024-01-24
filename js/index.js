3b821ada5f5d0e03a99564e7229d6f923ad9343a
Anton Yadchenko <antonfreedom5@gmail.com>
2024-01-24 21:22:19 +0300

    add images

--- js/index.js
+++ js/index.js
@@ -9,8 +9,25 @@ const machines = {
 tg.expand(); //расширяем на все окно
 
 tg.MainButton.text = "Сделать заказ"; //изменяем текст кнопки
-tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
-tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
+// tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
+// tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
 tg.MainButton.show();
 
+
+
 console.log(tg.initData);
+
+tg.onEvent('mainButtonClicked', function() {
+
+    tg.sendData(JSON.stringify(getData()));
+    tg.close();
+    //при клике на основную кнопку отправляем данные в строковом виде
+});
+
+function getData() {
+    const selectedMachine = document.querySelector('input[name="rate"]:checked').value;
+    const place = document.getElementById('place');
+    const phone = document.getElementById('phone');
+    const date = document.getElementById('date');
+    return { selectedMachine, place, phone, date };
+}
